996263dd6ed979d01bc20b0cd0946a421c2f1707
jhsim <jhsim@spectra.co.kr>
2022-12-04 22:41:36 +0900

    test commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,20 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void splitTest() {
+        String[] nums = "1,2".split(",");
+        assertThat(nums).contains("1");
+        assertThat(nums).containsExactly(new String[]{"1","2"});
+    }
+
+    @Test
+    void subStringTest() {
+        String nums = "(1,2)";
+        String newnums = nums.substring(1,3);
+        System.out.println(newnums);
+        //assertThat(newnums).isEqualTo("1,2");
     }
 }
